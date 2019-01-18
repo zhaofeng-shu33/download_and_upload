99242ac15c024656e99b2ca828ff7691f08cb782
zhaofeng-shu33 <616545598@qq.com>
2019-01-18 23:21:50 +0800

    pip install oss

--- upload.py
+++ upload.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
         auth = oss2.Auth(access_key_id, access_key_secret)
         bucket = oss2.Bucket(auth, 'http://oss-cn-shenzhen.aliyuncs.com', 'freiwilliger')
         # open `main.pdf` and `clustering.pdf` and upload them to the oss server
-        file = open(sys.arg[1], 'rb')
+        file = open(sys.argv[1], 'rb')
         research_base = 'tmp/'
         bucket.put_object(research_base + file, file)
